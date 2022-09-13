4521697b76c0ca90668d787a4ad927a78254e8b7
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-09-13 12:16:48 +0700

    first commit for second assignment

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_catalog_item,
+        'nama': 'Sasha Nabila Fortuna'
+    }
+    return render(request, "katalog.html", context)
